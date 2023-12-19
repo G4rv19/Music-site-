4af7d828706bc70da850ef5c84a0a43e29068c3c
G4rv19 <123931565+G4rv19@users.noreply.github.com>
2023-12-20 01:37:58 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+#importing Flask and templates for further use
 from flask import Flask, render_template
-
+# naming basic syntax
 app = Flask(__name__)
-
+#first route '/' or '/home' which will be homepage 
 @app.route('/')
 @app.route('/home')
+#this def function will further take it to stuff inside the page.
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html') # just an html template which will change the looks of webpage'/' aka homepage.
 
-@app.route('/about')
+@app.route('/about') # another webpage which is called 'about'
 def about():
-    return render_template('about.html')
+    return render_template('about.html') #importing another template which will render it stuff.
 
 @app.route('/login')
 def login():
@@ -21,3 +23,4 @@ def signup():
 
 if __name__ == '__main__':
     app.run(debug=True)
+#just last debugging syntax and website is done.
