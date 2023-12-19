8b17d6e29680ca71d17817bd73cdb6e1cec1dc62
G4rv19 <123931565+G4rv19@users.noreply.github.com>
2023-12-19 17:33:42 +1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
